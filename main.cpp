0758dbff59af888bbb806fa94e3efa28c7a1672a
kirillS1 <krya_kirya@mail.ru>
2025-12-17 19:03:06 +0700

    3

--- main.cpp
+++ main.cpp
@@ -4,14 +4,9 @@
 using namespace std;
 
 int main() {
-    // Настройка кодировки для Windows
-
     cout << "| Начало работы ЧИНАР |" << endl;
-
-    // Создание и запуск приложения
     CafeApp app;
     app.run();
-
     cout << "| Конец |" << endl;
     return 0;
 }
